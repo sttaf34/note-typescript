14892ebe7f449c5ad655e99cf5f62b474cf3667a
Shinichi Kawamura <sttaf34@gmail.com>
2020-02-26 19:49:18 +0900

    XPath のメモ追加

--- src/selenium/xpath.ts
+++ src/selenium/xpath.ts
@@ -6,7 +6,7 @@ class Driver {
   private setup = async (): Promise<void> => {
     const capabilities = Capabilities.chrome()
     capabilities.set("chromeOptions", {
-      args: [`--headless`]
+      // args: [`--headless`]
     })
     this.driver = await new Builder().withCapabilities(capabilities).build()
   }
@@ -17,14 +17,23 @@ class Driver {
     console.log("text -> ", text)
   }
 
+  // いろんな xpath の指定の仕方
+  // 取れるかどうかの試行錯誤は、Chrome DevTools > Console で
+  // $x("//*[@id='p-namespaces-label']") とかすると良い
   private doSomethingOnWikipedia = async (): Promise<void> => {
     await this.driver.get("https://ja.wikipedia.org/")
 
-    // idで取得
+    // idで完全一致で取得
     await this.log("//*[@id='p-namespaces-label']")
 
-    // classの場合、完全一致でなくて、含むか否かの判定になる
-    await this.log("//h1[contains(@class, 'firstHeading')]")
+    // class名の完全一致で取得
+    await this.log("//h1[@class='firstHeading']")
+
+    // class名の部分一致で取得
+    await this.log("//h1[contains(@class, 'firstHead')]")
+
+    // class名の完全一致、かつ、テキストの部分一致
+    await this.log("//a[@class='mw-jump-link' and contains(text(), '検索')]")
 
     // ルートからたどって取得
     await this.log("/html/body/div[5]/div[1]/div[2]/div[1]/ul/li[2]/a")
