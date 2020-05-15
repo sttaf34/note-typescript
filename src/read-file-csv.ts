25d6593b6ed9d940bc257516df36f464aaf71bd1
Shinichi Kawamura <sttaf34@gmail.com>
2020-05-15 15:38:07 +0900

    ストリームで CSV を読み込むメモ追加

--- src/read-file-csv.ts
+++ src/read-file-csv.ts
@@ -1,9 +1,34 @@
-import { readFileSync } from "fs"
+import * as parse from "csv-parse"
+
+import { readFileSync, createReadStream } from "fs"
 import { expandTilde } from "./useful-functions"
 
 import csvSync = require("csv-parse/lib/sync")
 
-const aPath = "~/Projects/note-typescript/assets/sample.csv"
-const data = readFileSync(expandTilde(aPath))
-const priceData = csvSync(data, { columns: true })
-console.log(priceData)
+// ファイルから一定量読みつつ、読んだ分から CSV のパーサーに渡している形
+{
+  const parser = parse((error, records) => {
+    console.log("ストリームの分")
+    console.log(records)
+  })
+
+  const aPath = "~/Projects/note-typescript/assets/sample.csv"
+  const stream = createReadStream(expandTilde(aPath))
+  stream.pipe(parser)
+}
+
+// CSV な形式の文字列をパースしている形
+{
+  const csvString = "1,2\n3,4\n"
+  parse(csvString, (error, records) => {
+    console.log(records)
+  })
+}
+
+// 同期的に読み込んでいる形
+{
+  const aPath = "~/Projects/note-typescript/assets/sample.csv"
+  const data = readFileSync(expandTilde(aPath))
+  const fruitData = csvSync(data, { columns: true })
+  console.log(fruitData)
+}
