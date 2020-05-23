661700351fdc2ee9ef30df2bb7d935f229e48b19
Shinichi Kawamura <sttaf34@gmail.com>
2020-05-23 18:54:50 +0900

    dayjs のメモ追加

--- src/datetime/dayjs.ts
+++ src/datetime/dayjs.ts
@@ -9,3 +9,14 @@ console.log(now)
 // フォーマットして文字列取得
 // https://day.js.org/docs/en/display/format
 console.log(now.format("YYYY-MM-DD HH:mm:ss"))
+
+// 時刻の計算
+console.log(now.subtract(1, "day")) // 1日過去
+console.log(now.add(1, "minute")) // 1分未来
+
+// ある日からある日数分の配列を取得
+const begin = now.subtract(1, "week")
+const dates = Array.from({ length: 7 }).map((_, index) => {
+  return begin.add(index, "day").format("YYYY-MM-DD")
+})
+console.log(dates)
