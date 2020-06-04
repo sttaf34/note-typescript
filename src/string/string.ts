984d77cd0587e2e4afc34b725643917d2db43825
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-04 20:19:32 +0900

    文字列置換のメモ追加

--- src/string/string.ts
+++ src/string/string.ts
@@ -1,29 +1,33 @@
 // 文字列埋め込み
-const stringA = (): void => {
+{
   const textA = "Hello"
   const textB = `${textA} World!`
   console.log(textB)
 }
-stringA()
 
 // 含むの判定
-const stringB = (): void => {
+{
   const textA = "pineapple"
   const result: boolean = textA.includes("apple")
   console.log(result)
 }
-stringB()
 
-// 置換
-const stringC = (): void => {
-  const textA = "92%"
+// 置換・先頭の一つだけ
+{
+  const textA = "92%100%"
   const textB = textA.replace("%", "")
   console.log(textB)
 }
-stringC()
+
+// 置換・全部
+{
+  const textA = "92%100%"
+  const textB = textA.replace(/%/g, "@@@")
+  console.log(textB)
+}
 
 // 開始文字列と終了文字列を指定してその前後を抜き出す
-const stringD = (): void => {
+{
   const html = "<html><body></body></html>"
   const start = html.indexOf("<body>")
   const end = html.indexOf("</body>", start) + "</body>".length
@@ -32,19 +36,16 @@ const stringD = (): void => {
   console.log(firstHalf)
   console.log(secondHalf)
 }
-stringD()
 
 // 文字列からインデックスで一文字取得
-const stringE = (): void => {
+{
   const textA = "pineapple"
   console.log(textA[2])
 }
-stringE()
 
 // 検索して個数を数える
-const stringF = (): void => {
+{
   const text = "pineapple apple grape orange"
-  const array = text.match(/apple/g) || []
+  const array: RegExpMatchArray = text.match(/apple/g) || []
   console.log(array.length)
 }
-stringF()
