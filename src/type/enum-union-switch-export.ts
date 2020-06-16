6ffa638a9f27937add35a2b01adee5d531d1e202
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-16 20:40:54 +0900

    列挙のメモの整理整頓

--- src/type/enum-union-switch-export.ts
+++ src/type/enum-union-switch-export.ts
@@ -1,3 +1,6 @@
+// 色々考慮して、列挙をどのように書くか考え中
+// https://www.kabuku.co.jp/developers/good-bye-typescript-enum
+
 // 文字列をそのまま enum っぽく使う場合
 export const fruits = [
   "apple",
@@ -16,3 +19,22 @@ export const Direction = {
   West: "W",
 } as const
 export type Direction = typeof Direction[keyof typeof Direction]
+
+// eslint(@typescript-eslint/switch-exhaustiveness-check) を有効にすることで、
+// switch 内での列挙漏れを防ぎ、eslint(default-case) は off にする
+//
+// case で return する場合は、eslint(consistent-return) 都度変更するしかない？
+// eslint-disable-next-line consistent-return
+const japaneseName = (direction: Direction): string => {
+  switch (direction) {
+    case Direction.North:
+      return "北"
+    case Direction.South:
+      return "南"
+    case Direction.East:
+      return "東"
+    case Direction.West:
+      return "西"
+  }
+}
+console.log(japaneseName(Direction.East))
