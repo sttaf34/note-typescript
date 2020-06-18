f38ed94df5487edddce5f4c450d91793cb4b1edb
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-18 19:31:50 +0900

    String.replace のメモ追加

--- src/string/string-replace.ts
+++ src/string/string-replace.ts
@@ -12,11 +12,42 @@
   console.log(textB)
 }
 
-// 置換・全部・文字列埋め込み
+// 置換・全部・文字列埋め込みで正規表現を組み立てる
 {
   const textA = "92%100%"
   const target = "%"
   const regularExpression = new RegExp(target, "g")
   const textB = textA.replace(regularExpression, "@@@")
   console.log(textB)
+}
+
+// 置換・先頭の一つだけ・関数で何か処理を書いて置換後の文字列にする場合
+{
+  const textA = "abcXXXdef"
+  const textB = textA.replace(/X/, (matched) => {
+    console.log(matched)
+    return String(matched.length)
+  })
+  console.log(textB)
+}
+
+// 置換・全部・関数で何か処理を書いて置換後の文字列にする場合
+{
+  const textA = "abcXXXdef"
+  const textB = textA.replace(/X/g, (matched) => {
+    return matched.toLowerCase()
+  })
+  console.log(textB)
+}
+
+// キャプチャを含んだ正規表現の場合
+{
+  const text = "&#12354;&#37326;&#37096;" // あ野部
+  const regex = /&#(\d+);/g
+  const replaced = text.replace(regex, (matched, capture) => {
+    console.log(matched) // &#12354; => &#37326; => &#37096;
+    console.log(capture) // 12354    => 37326    => 37096
+    return String.fromCharCode(capture) // 数値から文字列化
+  })
+  console.log(replaced)
 }
