31fc48e7b91e8e20aa8523b725a33ced5aea0899
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-18 19:47:35 +0900

    正規表現のメモ追加

--- src/basic/regular-expression.ts
+++ src/basic/regular-expression.ts
@@ -1,14 +1,24 @@
-const regularA = (): void => {
+{
   const text = "abcdefghijklmnopqrstuvwxyzabc"
   const result = text.match(/abc/)
   console.log(result) // 1個
 }
-regularA()
 
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Regular_Expressions#Advanced_Searching_With_Flags
-const regularB = (): void => {
+{
   const text = "abcdefghijklmnopqrstuvwxyzabc"
   const result = text.match(/abc/g) // g => グローバルサーチフラグ
   console.log(result) // 複数マッチすれば複数個返る
 }
-regularB()
+
+{
+  const text = "ABCDeFghiJKLMNoPQrsTu"
+  const result = text.match(/[a-z]/g) // アルファベット小文字
+  console.log(result)
+}
+
+{
+  const text = "吾輩は CAT である。名前は nothing だ。今年で 88 才です。"
+  const result = text.match(/[a-zA-z0-9]+/g) // アルファベットと数字
+  console.log(result)
+}
