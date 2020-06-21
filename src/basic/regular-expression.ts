e3b9f2e7705cb6b783e23e77194d87296c8f69e7
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-22 02:21:22 +0900

    正規表現のメモ追加

--- src/basic/regular-expression.ts
+++ src/basic/regular-expression.ts
@@ -22,3 +22,15 @@
   const result = text.match(/[a-zA-z0-9]+/g) // アルファベットと数字
   console.log(result)
 }
+
+{
+  const text = "https://example.com/aaa"
+
+  // ? => 直前文字・0個か1個・最長マッチ
+  // 全然きっちり対応していないドメイン部分取得
+  const result = text.match(/https?:\/\/([a-zA-z0-9-.]+)\/.*/)
+  console.log(result)
+
+  // ドメイン名のしくみ
+  // https://www.nic.ad.jp/ja/dom/system.html
+}
