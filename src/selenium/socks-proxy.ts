2fa647d8a14ce96e5b56f3839f4ac9fe7dd4f75a
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-23 20:07:40 +0900

    Selenium で SOCKS のメモ追加

--- src/selenium/socks-proxy.ts
+++ src/selenium/socks-proxy.ts
@@ -5,18 +5,17 @@ import { Capabilities, Builder } from "selenium-webdriver"
 const getCapabilities = (): Capabilities => {
   const capabilities = Capabilities.chrome()
   capabilities.set("chromeOptions", {
-    args: [
-      `--no-sandbox`,
-      `--disable-gpu`,
-      `--window-size=800,600`,
-      `--proxy-server=socks://localhost:10080`,
-    ],
+    args: [`--proxy-server=socks://localhost:10080`],
   })
   return capabilities
 }
 
 const main = async (): Promise<void> => {
-  const driver = await new Builder().withCapabilities(getCapabilities()).build()
+  const driver = new Builder()
+    .forBrowser("chrome")
+    .withCapabilities(getCapabilities())
+    .build()
   await driver.get("https://ifconfig.io/")
+  await driver.sleep(10 * 1000)
 }
 main()
