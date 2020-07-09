b9c28ac1653e76b174de8afe37c14c50aa841bea
Shinichi Kawamura <sttaf34@gmail.com>
2020-07-09 19:53:59 +0900

    正規表現のメモ追加

--- src/basic/regular-expression.ts
+++ src/basic/regular-expression.ts
@@ -26,11 +26,18 @@
 {
   const text = "https://example.com/aaa"
 
-  // ? => 直前文字・0個か1個・最長マッチ
+  // ? => 直前文字・0個か1個
   // 全然きっちり対応していないドメイン部分取得
   const result = text.match(/https?:\/\/([a-zA-z0-9-.]+)\/.*/)
   console.log(result)
 
   // ドメイン名のしくみ
   // https://www.nic.ad.jp/ja/dom/system.html
+}
+
+{
+  // URLの中の日付の部分取得
+  const text = "https://example.com/2020-07-07/"
+  const result = text.match(/.+?(\d+-\d+-\d+).+?/)
+  console.log(result)
 }
