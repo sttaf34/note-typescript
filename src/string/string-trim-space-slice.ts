26c120cc25126138e793f48771eb5fb40ddcbc8d
Shinichi Kawamura <sttaf34@gmail.com>
2020-08-17 20:00:23 +0900

    string のメモ追加

--- src/string/string-trim-space-slice.ts
+++ src/string/string-trim-space-slice.ts
@@ -6,4 +6,10 @@ console.log(`[${textA.trim()}]`)
 const textB = " Hello　"
 console.log(`[${textB.trim()}]`)
 
+// 切り取り
+const textC = "abcdefghijklmnopqrstuvwxyz"
+const beginIndex = 0
+const endIndex = 5
+console.log(textC.slice(beginIndex, endIndex))
+
 export {}
