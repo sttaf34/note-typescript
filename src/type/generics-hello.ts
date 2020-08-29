08e63bbeb95ee9003e8a9c84ee1aeabd3c2af169
Shinichi Kawamura <sttaf34@gmail.com>
2020-08-29 16:54:05 +0900

    ジェネリクスな関数の簡単な例を追加

--- src/type/generics-hello.ts
+++ src/type/generics-hello.ts
@@ -13,13 +13,25 @@ const fruit: Fruit<boolean> = {
 }
 console.log(fruit.feature)
 
-// ジェネリクスの意味がないけど、任意の型を渡せる関数
-// TODO: もうちょっと良い例に変更する
-const consoleLog = <T>(value: T): void => {
-  console.log(value)
+// 任意の型の配列において、インデックスが有効かどうか検証する関数
+const isValidArrayIndex = <T>(array: T[], index: number): boolean => {
+  if (Number.isInteger(index) === false) {
+    return false
+  }
+  if (index < 0 || array.length <= index) {
+    return false
+  }
+  return true
 }
 
-// 型を渡して実行
-consoleLog<number>(3)
+// 型を明示して実行
+console.log(isValidArrayIndex<number>([1, 2, 3], 0))
+
+// 型推論されるので書かなくても大丈夫
+// Show Hover すると number になってる
+console.log(isValidArrayIndex([1, 2, 3], 1))
+
+// Show Hover すると string | string になってる
+console.log(isValidArrayIndex(["Hey", 2, 3], 0))
 
 export {}
