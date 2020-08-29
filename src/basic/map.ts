4aeb1e52b820c7a9dc4b2471383a92361d73653a
Shinichi Kawamura <sttaf34@gmail.com>
2020-08-29 19:25:59 +0900

    map のメモを追加

--- src/basic/map.ts
+++ src/basic/map.ts
@@ -3,9 +3,17 @@
 // 基本
 const mapA = new Map<string, number>()
 mapA.set("keyA", 777)
+mapA.set("keyB", 888)
 console.log(mapA.get("keyA"))
 
 // 反復
 mapA.forEach((key, value) => {
   console.log(`${key} => ${value}`)
 })
+
+// キーのみを配列にする
+const keys: IterableIterator<string> = mapA.keys()
+const keyA = Array.from(keys).find((key) => key === "keyA")
+console.log(keyA)
+
+export {}
